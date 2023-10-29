90b7a105736e0ba4a40f8d80e11a3a5b43e95005
Breno de Angelo <brenodeangelo@gmail.com>
2023-10-29 16:35:15 -0300

    VAD

--- my_transcribe.py
+++ my_transcribe.py
@@ -1,10 +1,14 @@
 import speech_recognition as sr
 import argparse
 import sounddevice
+import torch
+import io
 
 from queue import Queue
 from sys import platform
 from time import sleep
+from pprint import pprint
+from tempfile import NamedTemporaryFile
 
 
 
@@ -30,7 +34,7 @@ def main():
                         help="Automatic calibration for microphone level.")  
     args = parser.parse_args()    
     
-
+    SAMPLE_RATE = 16000
     if 'linux' in platform:
         mic_name = args.default_microphone
         if not mic_name or mic_name == 'list':
@@ -41,10 +45,10 @@ def main():
         else:
             for index, name in enumerate(sr.Microphone.list_microphone_names()):
                 if mic_name in name:
-                    source = sr.Microphone(sample_rate=16000, device_index=index)
+                    source = sr.Microphone(sample_rate=SAMPLE_RATE, device_index=index)
                     break
     else:
-        source = sr.Microphone(sample_rate=16000)
+        source = sr.Microphone(sample_rate=SAMPLE_RATE)
 
 
     phrase_queue = Queue()
@@ -57,14 +61,38 @@ def main():
             print("Please wait. Calibrating microphone...")
             recorder.adjust_for_ambient_noise(source, duration=1)
     
+    torch.set_num_threads(1)
+    USE_ONNX = False
+    vad_model, vad_utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
+                              model='silero_vad',
+                              force_reload=False,
+                              onnx=USE_ONNX)
+    (get_speech_timestamps,
+    save_audio,
+    read_audio,
+    VADIterator,
+    collect_chunks) = vad_utils
+
+    temp_file = NamedTemporaryFile().name
     def record_callback(_, audio:sr.AudioData) -> None:
-        phrase_queue.put(audio)
-        print("New phrase added. Being processed...")
+        wav_data = io.BytesIO(audio.get_wav_data())
+        with open(temp_file, 'w+b') as f:
+            f.write(wav_data.read())
+        vad_audio = read_audio(temp_file, sampling_rate=SAMPLE_RATE)
+        speech_timestamps = get_speech_timestamps(vad_audio, vad_model, sampling_rate=SAMPLE_RATE)
+        # save_audio('only_speech.wav',
+        #    collect_chunks(speech_timestamps, vad_audio), sampling_rate=SAMPLE_RATE) 
+        pprint(speech_timestamps)
+        if speech_timestamps != []:
+            phrase_queue.put(audio)
+            print("New phrase added.")
     
     recorder.listen_in_background(source, record_callback, phrase_time_limit=args.record_timeout)
 
+    print("You can start talking")
     while True:
         if not phrase_queue.empty():
+            print('Processing...')
             audio_data = phrase_queue.get()
             transcript = recorder.recognize_whisper(audio_data, args.model, language=args.language)
             print(transcript)
